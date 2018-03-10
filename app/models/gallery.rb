4f864c1d4cd9ca405b2d180166ef71111c50c9d7
Brian Haughey <bhwoodworking@gmail.com>
2018-03-10 03:46:50 +0000

    Work on paperclip validations

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,7 +3,7 @@ class Gallery < ApplicationRecord
 
     has_attached_file :image, styles: { large: "800x800>", thumb: "100x100>" }  #, default_url: "/images/:style/missing.png"
     validates_attachment :image, presence: true,
-    content_type: { content_type: "image/jpeg" },
+    content_type: { content_type: ["image/jpeg", "image/jpg", "image/png"] },
     size: { in: 0..6000.kilobytes }
     
     
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,8 @@ class Picture < ApplicationRecord
   belongs_to :gallery
  
   has_attached_file :image, styles: { large: "800x800>", thumb: "100x100>" }  #, default_url: "/images/:style/missing.png"
-  validates_attachment :image, presence: true,
-    content_type: { content_type: "image/jpeg" },
+    validates_attachment :image, presence: true,
+    content_type: { content_type: ["image/jpeg", "image/jpg", "image/png"] },
     size: { in: 0..6000.kilobytes }
 
 
