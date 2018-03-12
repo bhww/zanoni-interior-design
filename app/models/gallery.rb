9374698bd8c35192235335d41bd9094db315577f
Brian Haughey <bhwoodworking@gmail.com>
2018-03-12 19:30:03 +0000

    work on galleries, pictures, etc

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,11 +1,15 @@
 class Gallery < ApplicationRecord
     has_many :pictures, dependent: :destroy
 
-    has_attached_file :image, styles: { large: "800x800>", thumb: "100x100>" }  #, default_url: "/images/:style/missing.png"
-    validates_attachment :image, presence: true,
-    content_type: { content_type: ["image/jpeg", "image/jpg", "image/png"] },
-    size: { in: 0..6000.kilobytes }
+    # has_attached_file :image, styles: { large: "800x800>", thumb: "100x100>" }  #, default_url: "/images/:style/missing.png"
+    # validates_attachment :image, 
+    # content_type: { content_type: ["image/jpeg", "image/jpg", "image/png"] },
+    # size: { in: 0..6000.kilobytes }
     
-    
-    
+    has_attached_file :image, 
+                    :styles => { :large => "1000x1000>", :thumb => ["150x150>", :png] }
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/JPG", "image/JPEG", "image/png", "image/PNG"]
+    validates_attachment_file_name :image, :matches => [/png\Z/, /PNG\Z/, /jpe?g\Z/, /JPE?G\Z/  ]
+    validates_attachment :image, size: { less_than: 1.megabytes }
+
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,9 +2,10 @@ class Picture < ApplicationRecord
   belongs_to :gallery
  
   has_attached_file :image, styles: { large: "800x800>", thumb: "100x100>" }  #, default_url: "/images/:style/missing.png"
-    validates_attachment :image, presence: true,
-    content_type: { content_type: ["image/jpeg", "image/jpg", "image/png"] },
-    size: { in: 0..6000.kilobytes }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/JPG", "image/JPEG", "image/png", "image/PNG"]
+     validates_attachment_file_name :image, :matches => [/png\Z/, /PNG\Z/, /jpe?g\Z/, /JPE?G\Z/, /pdf\Z/, /PDF\Z/  ]
+     validates_attachment :image, size: { less_than: 6.megabytes }
+     validates :image, :attachment_presence => true
 
 
 
